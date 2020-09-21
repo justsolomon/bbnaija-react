b622ff5250244598a6da785881486cfcaa241181
Gbolahan Balogun <gbolahanbalogun5@gmail.com>
2020-09-21 20:13:33 +0100

    Add two more housemates to the housemates array to complete it

--- src/components/HousematesList.js
+++ src/components/HousematesList.js
@@ -22,6 +22,18 @@ const staticHousemates = [
     picture:
       'https://ichef.bbci.co.uk/news/1024/branded_pidgin/8C52/production/_113522953_oda11.jpg',
   },
+  {
+    name: 'Nengi',
+    votes: 0,
+    picture:
+      'https://ichef.bbci.co.uk/news/640/cpsprodpb/9742/production/_113522783_one.jpg',
+  },
+  {
+    name: 'Vee',
+    votes: 0,
+    picture:
+      'https://ichef.bbci.co.uk/news/640/cpsprodpb/DD92/production/_113522765_8.jpg',
+  },
 ];
 
 export default staticHousemates;
