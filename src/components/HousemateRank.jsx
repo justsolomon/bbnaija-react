21e92a830ba1bf2e8c3b5642d01be31733b90ada
Gbolahan Balogun <gbolahanbalogun5@gmail.com>
2020-09-21 19:03:09 +0100

    Add jsx files for all leaderboard components

---
+++ src/components/HousemateRank.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function HousemateRank({ housemate }) {
+    return (
+        <div className='housemate-ranking'>
+            <div className="row">
+                <div className="col-3 my-auto">
+                    <img src={housemate.picture} alt=""/>
+                </div>
+                <div className="col-6 my-auto">
+                    <h5 className="housemate-ranking__name">{housemate.name}</h5>
+                </div>
+                <div className="col-3 d-flex justify-content-end">
+                    <div className="candidate-position d-flex align-items-center justify-content-center">
+                        <p className="candidate-position__number text-bold">1</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
