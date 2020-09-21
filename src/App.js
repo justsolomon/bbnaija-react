e44d290c6017c0cc3c5017ea4f4537db6ea83c86
Gbolahan Balogun <gbolahanbalogun5@gmail.com>
2020-09-21 13:42:51 +0100

    Move Voting and Leaderboard components into different routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./assets/scss/app.scss";
+import Leaderboard from "./components/Leaderboard";
 import Voting from "./components/Voting";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 export default function App() {
   return (
     <div className="home">
-      <Voting />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Voting} />
+          <Route path="/leaderboard" component={Leaderboard} />
+        </Switch>
+      </Router>
     </div>
   );
 }
