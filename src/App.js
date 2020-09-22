d77a325bf9335b39ac369dc8748f2221b2b469a5
Gbolahan Balogun <gbolahanbalogun5@gmail.com>
2020-09-22 14:38:05 +0100

    Use React Context to pass housemates list between pages
    This commit wraps the app component in a context provider component so
    the updated housemates array after a user has voted on the Vote page can
    be used to calculate each housemate's position on the leaderboard page.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './assets/scss/app.scss';
 import Leaderboard from './pages/Leaderboard';
 import Voting from './pages/Voting';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HousematesProvider } from './context/HousematesContext';
 
 export default function App() {
+  const [housemates, setHousemates] = useState({});
+
+  const updateHMContext = (newHMList) => setHousemates(newHMList);
+
   return (
     <div className='home'>
       <Router>
         <Switch>
-          <Route exact path='/' component={Voting} />
-          <Route path='/leaderboard' component={Leaderboard} />
+          <HousematesProvider value={{ housemates, updateHMContext }}>
+            <Route exact path='/' component={Voting} />
+            <Route path='/leaderboard' component={Leaderboard} />
+          </HousematesProvider>
         </Switch>
       </Router>
     </div>
