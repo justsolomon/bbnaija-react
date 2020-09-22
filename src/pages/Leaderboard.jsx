5db310254b515250a5675a7dcbe28eeb25d982a5
Gbolahan Balogun <gbolahanbalogun5@gmail.com>
2020-09-22 15:34:17 +0100

    Determine candidate positions on leaderboard
    This commit sorts the housemates array gotten from the context provider
    in descending order so the housemates votes reflect when rendering.
    
    Also determines the housemate that gets evicted and passes it as a
    dynamic state prop when rendering.

--- src/pages/Leaderboard.jsx
+++ src/pages/Leaderboard.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import HousemateRank from '../components/HousemateRank';
-import staticHousemates from '../components/HousematesList';
 import { useHistory } from 'react-router-dom';
+import HousematesContext from '../context/HousematesContext';
 
 export default function Leaderboard() {
+  const [hmPositions, setHmPositions] = useState([]);
+  const [evictedHM, setEvictedHM] = useState({});
+
+  const { housemates } = useContext(HousematesContext);
+
+  useEffect(() => {
+    //sort housemates array in descending order
+    housemates.sort((a, b) => b.votes - a.votes);
+
+    setHmPositions(housemates);
+    setEvictedHM(housemates[housemates.length - 1]);
+  }, []);
+
   const history = useHistory();
-  const housemateRanks = staticHousemates.map((hm) => (
+
+  const housemateRanks = hmPositions.map((hm, i) => (
     <div key={hm.name} className='col-md-6'>
-      <HousemateRank housemate={hm} />
+      <HousemateRank housemate={hm} rank={i + 1} />
     </div>
   ));
+
   return (
     <div className='leaderboard'>
       <div className='header text-center mb-3'>
@@ -19,7 +34,7 @@ export default function Leaderboard() {
       <div className='row'>{housemateRanks}</div>
 
       <div className='mt-5 text-center text-bold'>
-        <p className='eviction-notice'>Dorathy was evicted</p>
+        <p className='eviction-notice'>{evictedHM.name} was evicted</p>
       </div>
 
       <div className='text-center mt-5'>
